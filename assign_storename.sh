c37857f7e80a0c21b493b0c42a6bf0571f77519c
Chris Simpson <chris@myscribr.com>
2012-10-31 14:14:27 -0700

    Updated assign script

--- assign_storename.sh
+++ assign_storename.sh
@@ -1,16 +1,24 @@
 STORE_NAME=""
+DOMAIN_NAMES=""
 
 while true; do
     read -p "Your Store folder name (i.e.: mystore) :" STORE_NAME
-	break;
+        break;
+done
+
+while true; do
+    read -p "Domain names you want this store to respond to:" DOMAIN_NAMES
+        break;
 done
 
 echo "Updating config files"
-find . -name ":q!nginx.conf" -exec sed -i "s/\[storename\]/$STORE_NAME/g" '{}' \;
+find . -name "nginx.conf" -exec sed -i "s/\[storename\]/$STORE_NAME/g" '{}' \;
+find . -name "nginx.conf" -exec sed -i "s/\[domain-names\]/$DOMAIN_NAMES/g" '{}' \;
 find . -name "unicorn*" -exec sed -i "s/\[storename\]/$STORE_NAME/g" '{}' \;
 
 echo "Creating Symbolic links"
-ln -s /var/www/$STORE_NAME/config/nginx.conf $STORE_NAME
-ln -s /var/www/$STORE_NAME/config/unicorn_init.sh unicorn_$STORE_NAME
+rm -rf /etc/nginx/sites-enabled/default
+ln -s /var/www/$STORE_NAME/config/nginx.conf /etc/nginx/sites-enabled/$STORE_NAME
+ln -s /var/www/$STORE_NAME/config/unicorn_init.sh /etc/init.d/unicorn_$STORE_NAME
 
 echo "Completed"
