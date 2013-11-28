4481d4ba006de20a629b405f63a8529a7d2428e1
Chris Simpson <ctsimpson@gmail.com>
2013-11-27 20:44:41 -0800

    Bug fixes

--- assign_storename_shared.sh
+++ assign_storename_shared.sh
@@ -17,28 +17,18 @@ while true; do
         break;
 done
 echo "************************************"
-echo " This script now creates your       "
-echo " your binstub files as well         "
+echo "Updating config files"
 echo "************************************"
 echo ""
-echo ""
-echo "Updating config files"
+
 NEW_PATH="$(echo "$STORE_PATH/$STORE_NAME" | sed -e 's/[/&]/\\&/g')"
 find . -name "nginx.conf" -exec sed -i "s/\[storepath\]/$NEW_PATH/g" '{}' \;
 find . -name "nginx.conf" -exec sed -i "s/\[domain-names\]/$DOMAIN_NAMES/g" '{}' \;
 find . -name "unicorn.rb" -exec sed -i "s/\[storepath\]/$NEW_PATH/g" '{}' \;
 find . -name "restart_server.sh" -exec sed -i "s/\[storepath\]/$NEW_PATH/g" '{}' \;
 
-echo "Creating Symbolic links"
+echo "Creating Symbolic link for Nginx"
 rm -rf /etc/nginx/sites-enabled/default
 ln -nfs $STORE_PATH/shared/config/nginx.conf /etc/nginx/sites-enabled/$STORE_NAME
-
-echo "Updating permissions"
-chmod -R 775 $STORE_PATH/$STORE_NAME
-
-echo "Generate Binstubs"
-cd  $STORE_PATH/$STORE_NAME
-
-bundle install --binstubs
 
 echo "Completed"
